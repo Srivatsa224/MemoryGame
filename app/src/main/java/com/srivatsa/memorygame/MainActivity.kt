42f57d8f9e5d545bd6b5d01c55fcda5b18237d26
srivatsa224 <srivatsa224@gmail.com>
2020-05-14 21:30:08 +0530

    Completed game!

--- app/src/main/java/com/srivatsa/memorygame/MainActivity.kt
+++ app/src/main/java/com/srivatsa/memorygame/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import com.srivatsa.memorygame.R.drawable.*
+import java.text.FieldPosition
 
 private const val TAG="MainActivity"
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     //instance variable
     private lateinit var buttons: List<ImageButton>
     private lateinit var cards: List<MemoryCard>
+    private var indexOfSingleSelectedCard: Int?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,13 +37,70 @@ class MainActivity : AppCompatActivity() {
         buttons.forEachIndexed { index,button ->
             button.setOnClickListener {
                 Log.i(TAG,"Button Clicked")
-                val card=cards[index]
-                card.isFaceUp=!card.isFaceUp
-                //in-line if/else
-                button.setImageResource(if(card.isFaceUp)images[index] else ic_code)
-
-
+                //update models onClick and UI of the game
+                updateModels(index)
+                updateViews()
             }
+        }
+
+    }
+
+    private fun updateViews()
+    {
+        cards.forEachIndexed { index, card->
+            val button=buttons[index]
+            if(card.isMatch){
+                button.alpha=0.2f
+            }
+            button.setImageResource(if(card.isFaceUp)card.identifier else ic_code)
+        }
+    }
+
+    private fun updateModels(position: Int)
+    {
+        val card=cards[position]
+        //error checking
+        if (card.isFaceUp){
+            Toast.makeText(this,"Invalid move!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        //3 cases
+        //0 cards previously flipped  -> restore cards + flip over selected card
+        //1 cards previously flipped  -> flip over the selected card + check if same ID
+        //2 cards previously flipped  ->  restore cards + flip over selected cards
+        if (indexOfSingleSelectedCard==null)
+        {
+            // 0 or 2 cards sleected previously
+            restoreCards()
+            indexOfSingleSelectedCard=position
+        }
+        else
+        {
+            checkforMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard=null
+
+        }
+
+        card.isFaceUp=!card.isFaceUp
+    }
+
+    private fun restoreCards() {
+        for (card in cards)
+        {
+            if(!card.isMatch)
+            {
+                card.isFaceUp=false
+            }
+        }
+    }
+
+    private fun checkforMatch(position1: Int, position2: Int) {
+
+        if(cards[position1].identifier==cards[position2].identifier)
+        {
+            Toast.makeText(this,"It's a match!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatch=true
+            cards[position2].isMatch=true
         }
 
     }
