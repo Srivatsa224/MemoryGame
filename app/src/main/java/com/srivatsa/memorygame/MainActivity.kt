979bc7ce679e83b2469b9daca9783b0ea1f7af63
srivatsa224 <srivatsa224@gmail.com>
2020-05-17 11:02:02 +0530

    Fixed bug with reset button!

--- app/src/main/java/com/srivatsa/memorygame/MainActivity.kt
+++ app/src/main/java/com/srivatsa/memorygame/MainActivity.kt
@@ -10,107 +10,107 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.srivatsa.memorygame.R.drawable.*
 import java.text.FieldPosition
 
-private const val TAG="MainActivity"
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     //instance variable
     private lateinit var buttons: List<ImageButton>
     private lateinit var cards: List<MemoryCard>
-    private var indexOfSingleSelectedCard: Int?=null
-
+    private var indexOfSingleSelectedCard: Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val images= mutableListOf(csk, mi, dc, rcb)
+        val images = mutableListOf(csk, mi, dc, rcb)
         //Add each image place
         images.addAll(images)
         //making images random
         images.shuffle()
 
-        buttons= listOf(imageButton1, imageButton2,imageButton3,imageButton4,
-            imageButton5,imageButton6,imageButton7,imageButton8)
-            cards= buttons.indices.map{ index->
+        buttons = listOf(
+            imageButton1, imageButton2, imageButton3, imageButton4,
+            imageButton5, imageButton6, imageButton7, imageButton8
+        )
+        cards = buttons.indices.map { index ->
             MemoryCard(images[index])
         }
-        buttons.forEachIndexed { index,button ->
+        buttons.forEachIndexed { index, button ->
             button.setOnClickListener {
-                Log.i(TAG,"Button Clicked")
+                Log.i(TAG, "Button Clicked")
                 //update models onClick and UI of the game
                 updateModels(index)
                 updateViews()
             }
         }
         resetBtn.setOnClickListener {
-                // code to perform reset action
-
+            // code to perform reset action
             images.shuffle()
-            restoreCards()
-            updateViews()
-            }
-    }
+            cards = buttons.indices.map { index ->
+                MemoryCard(images[index])
 
-    private fun updateViews()
-    {
-        cards.forEachIndexed { index, card->
-            val button=buttons[index]
-            if(card.isMatch){
-                button.alpha=0.2f
             }
-            button.setImageResource(if(card.isFaceUp)card.identifier else ipl)
+            updateViews()
         }
     }
 
-    private fun updateModels(position: Int)
-    {
-        val card=cards[position]
+    private fun updateViews() {
+        cards.forEachIndexed { index, card ->
+            val button = buttons[index]
+            if (card.isMatch) {
+                button.alpha = 0.2f
+            }
+            else
+            {
+                button.alpha=1.0f
+            }
+            button.setImageResource(if (card.isFaceUp) card.identifier else ipl)
+        }
+    }
+
+    private fun updateModels(position: Int) {
+        val card = cards[position]
         //error checking
-        if (card.isFaceUp){
-            Toast.makeText(this,"Invalid move!", Toast.LENGTH_SHORT).show()
+        if (card.isFaceUp) {
+            
+            Toast.makeText(this, "Invalid move!", Toast.LENGTH_SHORT).show()
             return
         }
         //3 cases
         //0 cards previously flipped  -> restore cards + flip over selected card
         //1 cards previously flipped  -> flip over the selected card + check if same ID
         //2 cards previously flipped  ->  restore cards + flip over selected cards
-        if (indexOfSingleSelectedCard==null)
-        {
+        if (indexOfSingleSelectedCard == null) {
             // 0 or 2 cards sleected previously
             restoreCards()
-            indexOfSingleSelectedCard=position
-        }
-        else
-        {
+            indexOfSingleSelectedCard = position
+        } else {
             checkforMatch(indexOfSingleSelectedCard!!, position)
-            indexOfSingleSelectedCard=null
+            indexOfSingleSelectedCard = null
 
         }
 
-        card.isFaceUp=!card.isFaceUp
+        card.isFaceUp = !card.isFaceUp
     }
 
     private fun restoreCards() {
-        for (card in cards)
-        {
-            if(!card.isMatch)
-            {
-                card.isFaceUp=false
+        for (card in cards) {
+            if (!card.isMatch) {
+                card.isFaceUp = false
             }
         }
     }
 
     private fun checkforMatch(position1: Int, position2: Int) {
 
-        if(cards[position1].identifier==cards[position2].identifier)
-        {
+        if (cards[position1].identifier == cards[position2].identifier) {
 
 
-            Toast.makeText(this,"It's a match!", Toast.LENGTH_SHORT).show()
-            cards[position1].isMatch=true
-            cards[position2].isMatch=true
+            Toast.makeText(this, "It's a match!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatch = true
+            cards[position2].isMatch = true
         }
 
     }
